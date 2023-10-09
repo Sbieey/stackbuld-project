5ce12fb54be04900b9a699e09232e91fd086fbd8
Muhammad Suleiman <brainbox.sb@gmail.com>
2023-10-09 09:22:03 +0100

    new-commit

--- src/app/components/SearchInput.tsx
+++ src/app/components/SearchInput.tsx
@@ -35,10 +35,11 @@ const SearchInput =  () => {
     // const []
 
 
-    const debouncedSearchTerm = useDebounce(search, 200)  
+    const debouncedSearchTerm = useDebounce(search, 200)
+    //@ts-ignore
     let {data:searchPost, isLoading:isloadingSearch, error} = useQuery({
         queryKey: ['search', debouncedSearchTerm], 
-        
+        //@ts-ignore
         queryFn: async () => {
           const posts:any = await getPosts()
             if(debouncedSearchTerm){
